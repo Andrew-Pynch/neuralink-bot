d1ab5a548b51297d5703e51ff280ef8f254e7406
Andrew Pynch <andrewpynchbusiness@gmail.com>
2020-03-19 16:24:58 -0700

    Lesson 1 q learning

--- misc/jpg_from_mp4.py
+++
@@ -1,9 +0,0 @@
-import cv2
-vidcap = cv2.VideoCapture('neuralink.mp4')
-success,image = vidcap.read()
-count = 0
-while success:
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
