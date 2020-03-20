27166617d1fffdbdc960a2453f593f58d7297255
Andrew Pynch <andrewpynchbusiness@gmail.com>
2020-03-20 15:21:26 -0700

    add correct gifs

---
+++ RL/QLearning/surgery_sim.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.remove('/opt/ros/kinetic/lib.python2.7/dist-packages')
+
+import time 
+import pickle 
+import cv2 
+import numpy as np
+from PIL import image 
+import matplotlib.pyplot as plt 
+from matplotlib import style
+
+style.use('ggplot')
+
+######################
+# SIMULATION CONSTANTS
+######################
+
+SIZE = ##########################
+
+NUM_EPISODES = 25000
+MOVE_PENALTY = 
+
